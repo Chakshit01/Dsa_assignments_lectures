3bef3eb1833ef97b3b71677da4edf5810eca8425
Chakshit <chakshitdhandli22@gmail.com>
2022-11-04 11:19:18 -0700

    binarySearch in 2D has been updated

--- company/lecture9/binarySearchIn2D.java
+++ company/lecture9/binarySearchIn2D.java
@@ -1,7 +1,41 @@
 package com.company.lecture9;
 
+import java.util.Arrays;
+
 public class binarySearchIn2D {
     public static void main(String[] args) {
-        
+        System.out.println("Binary search in 2d Arrays");
+        int [][] matrix ={
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}
+
+        };
+        int target =35;
+        System.out.println(Arrays.toString(search(matrix,target)));
     }
+    static int [] search(int [][] matrix, int target)
+    {
+        int row =0;
+        int col =matrix.length-1;
+
+        while (row<matrix.length && col >=0)
+        {
+            if (matrix[row][col]==target)
+            {
+                return new  int []{row,col};
+            }
+            if (matrix[row][col]<target)
+            {
+                row++;
+            }
+            else if (matrix[row][col]>target)
+            {
+                col--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
 }
