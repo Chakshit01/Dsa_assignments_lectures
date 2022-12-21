ae8eb9548f98124cf907b661d50fb668040c1103
Chakshit <chakshitdhandli22@gmail.com>
2022-12-21 10:03:35 -0800

    new practice question solved

---
+++ practice/indexIninfiniteArray.java
@@ -0,0 +1,9 @@
+package com.practice;
+
+public class indexIninfiniteArray {
+    public static void main(String[] args) {
+        System.out.println("Finding the position of an element in the infinite Array");
+
+    }
+
+}
