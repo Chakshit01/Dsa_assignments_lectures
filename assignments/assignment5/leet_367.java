7e0aa470c195925bf10980adced2f7f081664c55
Chakshit <chakshitdhandli22@gmail.com>
2023-03-02 23:48:22 +0530

    new questions solved

--- assignments/assignment5/leet_367.java
+++ assignments/assignment5/leet_367.java
@@ -1,2 +1,24 @@
 package com.assignments.assignment5;public class leet_367 {
+
+    public static void main(String[] args) {
+        System.out.println("find the valid square");
+        int n=16;
+        System.out.println(isPerfectSquare(n));;
+
+    }
+
+    static boolean isPerfectSquare(int num) {
+        int low = 1, high = num;
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = (int) mid + 1;
+            } else {
+                high = (int) mid - 1;
+            }
+        }
+        return false;
+    }
 }
---
+++ assignments/assignment5/leet_441.java
@@ -0,0 +1,18 @@
+package com.assignments.assignment5;
+
+public class leet_441 {
+    public static void main(String[] args) {
+        System.out.println("Arranging coins");
+        int n = 15;
+        System.out.println(arrangeCoins(n)+" rows completed");
+    }
+
+    static int arrangeCoins(int n) {
+        int i = 1; // which row we are on
+        while(n > 0){ // checking to see if we have used all our coins
+            i++; // increasing our row
+            n = n-i; // adding coins to our row
+        }
+        return i-1; // we return our current row minus one because the last row i
+    }
+}
